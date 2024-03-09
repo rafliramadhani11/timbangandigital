b7949ad6d309d0a35c1e6a3c6315abdd01e9d154
Rafli Ramadhani <rafliramadhani438@gmail.com>
2024-03-09 16:39:21 +0700

    test branch

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,6 +6,7 @@
     @include('partials.sidebar')
     <div id="main-content" class="relative w-full h-full overflow-y-auto bg-gray-50 lg:ml-64 dark:bg-gray-900">
         <main class="px-4 py-6">
+            test
             <!-- BAR CHART -->
             @if (count($regionsUser) > 0)
             <div class="p-4 px-4 mb-6 bg-white rounded shadow-md dark:bg-gray-800 md:p-8">
