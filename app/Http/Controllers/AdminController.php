3382368a17851fe68e69f01136756b0679d739e6
Rafli Ramadhani <rafliramadhani438@gmail.com>
2024-03-11 07:49:27 +0700

    done User user.index

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -73,62 +73,7 @@ class AdminController extends Controller
         return redirect()->route('admin.users')->with('stored', 'Data baru telah di buat !');
     }
 
-    public function storeAnak($username, Request $request)
-    {
-        $user = User::where('username', $username)->first();
-        $request->validate([
-            'name' => 'required',
-            'jeniskelamin' => 'required',
-            'umur' => 'required',
 
-            'pb' => 'required',
-            'bb' => 'required',
-        ]);
-        $anak = new Anak([
-            'user_id' => $user->id,
-            'name' => $request->input('name'),
-            'jeniskelamin' => $request->input('jeniskelamin'),
-        ]);
-        $anak->save();
-
-        // IMT RUMUS
-        $umur = $request->input('umur');
-
-        $pb = $request->input('pb');
-        $bb = $request->input('bb');
-        $pbMeter = $pb / 100;
-
-        $imt =  $bb / ($pbMeter * $pbMeter);
-        // ---------------------------------------
-
-
-        // FUZZY
-        $imt_status = ($anak->jeniskelamin == 'Laki Laki')
-            ? fuzzy_imt_usia($umur, round($imt, 1), true)
-            : fuzzy_imt_usia($umur, round($imt, 1), false);
-
-        $pb_status = ($anak->jeniskelamin == 'Laki Laki')
-            ? fuzzy_tb_usia($umur, $pb, true)
-            : fuzzy_tb_usia($umur, $pb, false);
-
-        $bb_status = ($anak->jeniskelamin == 'Laki Laki')
-            ? fuzzy_bb_usia($umur, $bb, true)
-            : fuzzy_bb_usia($umur, $bb, false);
-        // -----------------------------------
-
-        $dataTimbangan = [
-            'anak_id' => $anak->id,
-            'umur' => $request->input('umur'),
-            'imt_status' => strtoupper($imt_status),
-            'pb_status' => strtoupper($pb_status),
-            'bb_status' => strtoupper($bb_status),
-            'pb' => $pb,
-            'bb' => $bb,
-            'imt' =>  round($imt, 1)
-        ];
-        Timbangan::where('anak_id', null)->update($dataTimbangan);
-        return redirect()->back()->with('storedAnak', 'Berhasil menambah data anak !');
-    }
 
 
     public function allUsers()
@@ -197,14 +142,7 @@ class AdminController extends Controller
         }
     }
 
-    public function updateAnak($id, Request $request)
-    {
-        DB::table('anaks')
-            ->where('id', $id)
-            ->update(['name' => $request->input('name')]);
 
-        return redirect()->back()->with('updatedName', 'Berhasil memperbarui nama !');
-    }
 
     public function delete($username)
     {
@@ -213,11 +151,7 @@ class AdminController extends Controller
         return redirect()->route('admin.users')->with('deleted', 'Data berhasil di hapus !');
     }
 
-    public function deleteAnak($id)
-    {
-        Anak::where('id', $id)->delete();
-        return redirect()->back()->with('deletedAnak', 'Data berhasil di hapus');
-    }
+
 
     public function logout(Request $request)
     {
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -43,7 +43,9 @@
                             Grafik perkembangan gizi anak
                         </span>
                     </div>
-                    <a class="focus:outline-none text-white bg-[#00A688]  focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600  dark:focus:ring-green-800">NORMAL</a>
+                    <a class="focus:outline-none text-white bg-[#00A688]  focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600  dark:focus:ring-green-800">
+                        NORMAL
+                    </a>
                 </div>
                 <div>
                     <div id="imtchart" class="mt-5"></div>
